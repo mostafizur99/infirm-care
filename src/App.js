e563528bbf1bf1c4cdb8040241feb624b983b98f
mostafizur99 <coder.mr11@gmail.com>
2021-10-20 21:37:27 +0600

    page scrolled top added

--- src/App.js
+++ src/App.js
@@ -16,40 +16,43 @@ import NotFound from './Pages/NotFound/NotFound';
 import Footer from './Pages/Shared/Footer/Footer';
 import Header from './Pages/Shared/Header/Header';
 import Team from './Pages/Team/Team';
+import ScrollToTop from './utilities/ScrollToTop';
 
 function App() {
   return (
     <div className="App">
       <AuthProvider>
         <Router>
-          <Header></Header>
-          <Switch>
-            <Route path="/home">
-              <Home></Home>
-            </Route>
-            <PrivateRoute path="/team">
-              <Team></Team>
-            </PrivateRoute>
-            <PrivateRoute path="/appointment">
-              <Appointment></Appointment>
-            </PrivateRoute>
-            <Route path="/login">
-              <Login></Login>
-            </Route>
-            <Route path="/sign-up">
-              <SignUp></SignUp>
-            </Route>
-            <PrivateRoute path="/service/:serviceId">
-              <ServiceDetail></ServiceDetail>
-            </PrivateRoute>
-            <Route exact path="/">
-              <Home></Home>
-            </Route>
-            <Route path="*">
-              <NotFound></NotFound>
-            </Route>
-          </Switch>
-          <Footer></Footer>
+          <ScrollToTop>
+            <Header></Header>
+            <Switch>
+              <Route path="/home">
+                <Home></Home>
+              </Route>
+              <PrivateRoute path="/team">
+                <Team></Team>
+              </PrivateRoute>
+              <PrivateRoute path="/appointment">
+                <Appointment></Appointment>
+              </PrivateRoute>
+              <Route path="/login">
+                <Login></Login>
+              </Route>
+              <Route path="/sign-up">
+                <SignUp></SignUp>
+              </Route>
+              <PrivateRoute path="/service/:serviceId">
+                <ServiceDetail></ServiceDetail>
+              </PrivateRoute>
+              <Route exact path="/">
+                <Home></Home>
+              </Route>
+              <Route path="*">
+                <NotFound></NotFound>
+              </Route>
+            </Switch>
+            <Footer></Footer>
+          </ScrollToTop>
         </Router>
       </AuthProvider>
     </div>
